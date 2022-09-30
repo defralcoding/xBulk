38de9b19bdd6278fb838451ca8ba5e1fba7dc5b1
Alessandro <defranceschi_a@yahoo.com>
2022-09-30 12:48:51 +0200

    NFT Distribution

--- snippets.sh
+++ snippets.sh
@@ -11,9 +11,34 @@ deploy() {
 }
 
 upgrade() {
-    erdpy --verbose contract upgrade erd1qqqqqqqqqqqqqpgqmurlyyz9cg04cuwxns85y73z2zse0ls94jwsf0js2m --project=${PROJECT} \
+    erdpy --verbose contract upgrade erd1qqqqqqqqqqqqqpgq7qdqsuq4a4pga2hxa5h0gvluf7hlc6hndn3q32jth5 --project=${PROJECT} \
     --recall-nonce --pem=${USER_PEM} \
     --gas-limit=50000000 \
     --send --outfile="deploy.interaction.json" \
     --proxy=${PROXY} --chain=${CHAIN_ID} || return
+}
+
+upgrade_devnet() {
+    erdpy --verbose contract upgrade erd1qqqqqqqqqqqqqpgq7qdqsuq4a4pga2hxa5h0gvluf7hlc6hndn3q32jth5 --project=${PROJECT} \
+    --recall-nonce \
+    --ledger --ledger-address-index 3 \
+    --gas-limit=50000000 \
+    --send --outfile="deploy.interaction.json" \
+    --proxy="https://devnet-gateway.elrond.com" --chain=D || return
+}
+upgrade_testnet() {
+    erdpy --verbose contract upgrade erd1qqqqqqqqqqqqqpgq5j3wahajwehwja70v39074zzzjsq89lkdn3qp3j2f9 --project=${PROJECT} \
+    --recall-nonce \
+    --ledger --ledger-address-index 3 \
+    --gas-limit=50000000 \
+    --send --outfile="deploy.interaction.json" \
+    --proxy="https://testnet-gateway.elrond.com" --chain=T || return
+}
+upgrade_mainnet() {
+    erdpy --verbose contract upgrade erd1qqqqqqqqqqqqqpgq5j3wahajwehwja70v39074zzzjsq89lkdn3qp3j2f9 --project=${PROJECT} \
+    --recall-nonce \
+    --ledger --ledger-address-index 3 \
+    --gas-limit=50000000 \
+    --send --outfile="deploy.interaction.json" \
+    --proxy="https://gateway.elrond.com" --chain=1 || return
 }
