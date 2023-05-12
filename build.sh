e05db2294bdcf55af1c514d2b55ae9dc122acc0e
Alessandro <defranceschi_a@yahoo.com>
2023-05-12 17:46:52 +0200

    update build

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-python3 build_with_docker.py --image="multiversx/sdk-rust-contract-builder:v4.1.4" \
+python3 build_with_docker.py --image="multiversx/sdk-rust-contract-builder:next" \
     --project="~/Documents/programmazione/DefralCoding/xBulk" \
     --output="~/Documents/programmazione/DefralCoding/xBulk/output"
--- snippets.sh
+++ snippets.sh
@@ -28,18 +28,18 @@ getgenerico() {
 }
 
 upgrade_devnet() {
-    mxpy --verbose contract upgrade erd1qqqqqqqqqqqqqpgq7qdqsuq4a4pga2hxa5h0gvluf7hlc6hndn3q32jth5 --project=${PROJECT} \
+    mxpy --verbose contract upgrade erd1qqqqqqqqqqqqqpgq7qdqsuq4a4pga2hxa5h0gvluf7hlc6hndn3q32jth5 --bytecode="output/xbulk/xbulk.wasm" \
     --recall-nonce \
     --ledger --ledger-address-index 3 \
-    --gas-limit=50000000 \
+    --gas-limit=40000000 \
     --send \
     --proxy="https://devnet-gateway.multiversx.com" --chain=D || return
 }
 upgrade_testnet() {
-    mxpy --verbose contract upgrade erd1qqqqqqqqqqqqqpgq5j3wahajwehwja70v39074zzzjsq89lkdn3qp3j2f9 --project=${PROJECT} \
+    mxpy --verbose contract upgrade erd1qqqqqqqqqqqqqpgq5j3wahajwehwja70v39074zzzjsq89lkdn3qp3j2f9 --bytecode="output/xbulk/xbulk.wasm" \
     --recall-nonce \
     --ledger --ledger-address-index 3 \
-    --gas-limit=50000000 \
+    --gas-limit=40000000 \
     --send \
     --proxy="https://testnet-gateway.multiversx.com" --chain=T || return
 }
@@ -73,18 +73,18 @@ upgrade_mainnet_all() {
 
 verify_mainnet_all() {
     mxpy --verbose contract verify "erd1qqqqqqqqqqqqqpgqtnksqd6rs4a74mf4un5h2w2tt5lanfmaayxqs35jth" \
-    --packaged-src=./output/xbulk/xbulk-1.0.0.source.json --verifier-url="https://play-api.multiversx.com" \
-    --docker-image="multiversx/sdk-rust-contract-builder:v4.1.2" \
+    --packaged-src=./output/xbulk/xbulk-2.0.0.source.json --verifier-url="https://play-api.multiversx.com" \
+    --docker-image="multiversx/sdk-rust-contract-builder:next" \
     --ledger --ledger-address-index 3
 
     mxpy --verbose contract verify "erd1qqqqqqqqqqqqqpgqwcv369k9x49ve3qlu0h5qe949w7m6gcxh42scqtdpf" \
-    --packaged-src=./output/xbulk/xbulk-1.0.0.source.json --verifier-url="https://play-api.multiversx.com" \
-    --docker-image="multiversx/sdk-rust-contract-builder:v4.1.2" \
+    --packaged-src=./output/xbulk/xbulk-2.0.0.source.json --verifier-url="https://play-api.multiversx.com" \
+    --docker-image="multiversx/sdk-rust-contract-builder:next" \
     --ledger --ledger-address-index 3
     
     mxpy --verbose contract verify "erd1qqqqqqqqqqqqqpgq5j3wahajwehwja70v39074zzzjsq89lkdn3qp3j2f9" \
-    --packaged-src=./output/xbulk/xbulk-1.0.0.source.json --verifier-url="https://play-api.multiversx.com" \
-    --docker-image="multiversx/sdk-rust-contract-builder:v4.1.2" \
+    --packaged-src=./output/xbulk/xbulk-2.0.0.source.json --verifier-url="https://play-api.multiversx.com" \
+    --docker-image="multiversx/sdk-rust-contract-builder:next" \
     --ledger --ledger-address-index 3 || return
 }
 
